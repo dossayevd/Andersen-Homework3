8ab3282e4061a6bf947ea8937b4f15d1e3658218
dossayevd <101496490+dossayevd@users.noreply.github.com>
2022-05-12 11:22:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+/* 1 Задача */
+Array.prototype.myFilter = function (callback, context) {
+  let arr = [];
+
+  for (i = 0; i < this.length; i++) {
+    if (callback.call(context, this[i], i, this)) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+};
+
+let arrData = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let newArr = arrData.myFilter((num) => {
+  return num > 4;
+});
+
+console.log(newArr);
+
+/* 2 Задача */
+function createDebounceFunction(fn, delay) {
+  let timeout;
+  return function () {
+    const func = () => {
+      fn.apply(this, arguments);
+    };
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(func, delay);
+  };
+}
+
+function hello() {
+  alert('Hello!');
+}
+
+hello = createDebounceFunction(hello, 3000);
+
+document.getElementById('btn').addEventListener('click', hello);
